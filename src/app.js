9c62876e4f29fde9623c8313d08d9cbbf3e1514c
Septiyan Nariyanto <septiyan.nariyanto@gmail.com>
2019-04-14 19:09:47 +0700

    Logger implementation.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+import logger from 'morgan';
 import { connect } from './config/db';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 connect();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'));
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT http://localhost:${PORT}`);
