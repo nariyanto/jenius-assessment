6dbb4bf884827bf0e3004d1399c609729af24978
Septiyan Nariyanto <septiyan.nariyanto@gmail.com>
2019-04-14 19:04:40 +0700

    DB connections.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+import { connect } from './config/db';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+connect();
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT http://localhost:${PORT}`);
