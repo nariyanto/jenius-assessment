9073c9ceaff4f8e968f63ff75364641072c134b4
Septiyan Nariyanto <septiyan.nariyanto@gmail.com>
2019-04-14 19:26:43 +0700

    Create user model.

---
+++ src/api/resources/user/user.model.js
@@ -0,0 +1,43 @@
+let mongoose = require('mongoose');
+
+export const USER_ROLE = 2;
+export const ADMIN_ROLE = 1;
+const { Schema } = mongoose;
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  accountNumber: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  emailAddress: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: (value) => {
+      return validator.isEmail(value)
+    }
+  },
+  identityNumber: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    default: 2,
+    required: true,
+    type: Number,
+  },
+});
+
+export default mongoose.model('User', userSchema);
